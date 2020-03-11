97ba0d26d01e4e805fe04f12abc39742055c1aee
jinbread <jinjae.lee@icloud.com>
2020-03-11 22:14:51 +0900

    first commit of apollo testing app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import { ApolloProvider } from "@apollo/react-hooks";
+import Continents from "./Continents";
 import './App.css';
+
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+
+const client = new ApolloClient({
+  link: createHttpLink({ uri: "https://countries.trevorblades.com" }),
+  cache: new InMemoryCache()
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <h1>React + Apollo Hooks</h1>
+      <Continents />
+    </ApolloProvider>
   );
 }
 
---
+++ src/Continents.js
@@ -0,0 +1,38 @@
+import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_CONTINENTS = gql`
+  query {
+    continents {
+      code
+      name
+    }
+  }
+`;
+
+const GET_COUNTRIES = gql`
+  query {
+    countries {
+      code
+      name
+    }
+  }
+`;
+
+
+
+function Continents() {
+  const { loading, error, data } = useQuery(GET_COUNTRIES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!(</p>;
+  return (
+    <ul>
+      {data.countries.map(({ code, name }) => (
+        <li key={code}>{name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default Continents;
